d144cbc96f0faa3298e86501f0aef7b11e766cb8
GN <sharmashalu3148@gmail.com>
2024-10-22 09:30:41 +0530

    ...

--- lib/view/song_play_page.dart
+++ lib/view/song_play_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:media_player1/service/song_service_page.dart';
 
 class SongPlayPage extends StatefulWidget {
@@ -15,28 +16,72 @@ class _SongPlayPageState extends State<SongPlayPage> {
       appBar: AppBar(
         title: const Text('Song Page'),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body:
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 28),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            IconButton(
-              onPressed: () async {
-                await AudioService.player.play();
-              },
-              icon: Icon(Icons.play_arrow,size: 35,),
+            Center(
+              child: Container(
+                height: 360,
+                width: 370,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(''),
+                  ),
+                ),
+              ),
             ),
-            IconButton(
-              onPressed: () async {
-                await AudioService.player.pause();
-
-              },
-              icon: Icon(Icons.pause,size: 35,),
+            const SizedBox(
+              height: 30,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Padding(
+                  padding:  EdgeInsets.only(left: 10),
+                  child: Text(
+                    'title',
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: IconButton(
+                    icon: Icon(
+                       Icons.favorite,
+                      size: 29,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {
+                      // Provider.of<SongProvider>(context,listen: false).toggleFavorite();
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            // slider(context, songProvider),
+            // sliderText(songProvider),
 
+            const SizedBox(
+              height: 15,
+            ),
+            // icons(songProvider),
           ],
         ),
       ),
     );
   }
 }
+
 int sliderValue = 0;
